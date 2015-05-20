63e1f1861b40cd8c4922fdd68ae9646f84eb1cfe
Michael Romero <gitcomments@dimensionsix.net>
2015-05-20 10:15:33 -0400

    rename navwalker functions, add helper function

--- modules/navwalker.php
+++ modules/navwalker.php
@@ -2,7 +2,7 @@
 
 namespace D6D\Sagextras\Navwalker;
 /**
- * Restore the Roots Bootstrap Navwalker for a cleaner Bootstrap menu 
+ * Restore the sagextra Bootstrap Navwalker for a cleaner Bootstrap menu 
  *
  */
 class Sagextra_Nav_Walker extends Walker_Nav_Menu {
@@ -34,7 +34,7 @@ class Sagextra_Nav_Walker extends Walker_Nav_Menu {
     elseif (stristr($item_html, 'li class="dropdown-header')) {
       $item_html = preg_replace('/<a[^>]*>(.*)<\/a>/iU', '$1', $item_html);
     }
-    $item_html = apply_filters('roots_wp_nav_menu_item', $item_html);
+    $item_html = apply_filters('sagextra_wp_nav_menu_item', $item_html);
     $output .= $item_html;
   }
   function display_element($element, &$children_elements, $max_depth, $depth = 0, $args, &$output) {
@@ -42,7 +42,7 @@ class Sagextra_Nav_Walker extends Walker_Nav_Menu {
     if ($element->is_dropdown) {
       $element->classes[] = 'dropdown';
       foreach ($children_elements[$element->ID] as $child) {
-        if ($child->current_item_parent || roots_url_compare($this->archive, $child->url)) {
+        if ($child->current_item_parent || url_compare($this->archive, $child->url)) {
           $element->classes[] = 'active';
         }
       }
@@ -57,7 +57,7 @@ class Sagextra_Nav_Walker extends Walker_Nav_Menu {
     $slug = sanitize_title($item->title);
     if ($this->cpt) {
       $classes = str_replace('current_page_parent', '', $classes);
-      if (roots_url_compare($this->archive, $item->url)) {
+      if (url_compare($this->archive, $item->url)) {
         $classes[] = 'active';
       }
     }
@@ -72,21 +72,36 @@ class Sagextra_Nav_Walker extends Walker_Nav_Menu {
  * Clean up wp_nav_menu_args
  *
  * Remove the container
- * Use Roots_Nav_Walker() by default
+ * Use Sagextra_Nav_Walker() by default
  * Remove the id="" on nav menu items
  */
-function roots_nav_menu_args($args = '') {
-  $roots_nav_menu_args['container'] = false;
+function sagextra_nav_menu_args($args = '') {
+  $sagextra_nav_menu_args['container'] = false;
   if (!$args['items_wrap']) {
-    $roots_nav_menu_args['items_wrap'] = '<ul class="%2$s">%3$s</ul>';
+    $sagextra_nav_menu_args['items_wrap'] = '<ul class="%2$s">%3$s</ul>';
   }
   if (current_theme_supports('bootstrap-top-navbar') && !$args['depth']) {
-    $roots_nav_menu_args['depth'] = 2;
+    $sagextra_nav_menu_args['depth'] = 2;
   }
   if (!$args['walker']) {
-    $roots_nav_menu_args['walker'] = new Sagextras_Nav_Walker();
+    $sagextra_nav_menu_args['walker'] = new Sagextras_Nav_Walker();
   }
-  return array_merge($args, $roots_nav_menu_args);
+  return array_merge($args, $sagextra_nav_menu_args);
 }
-add_filter('wp_nav_menu_args', 'roots_nav_menu_args');
+
+/**
+ * URL Compare helper class, it's needed for the code to work.
+ * Found that out the hard way once :)
+ */
+function url_compare($url, $rel) {
+  $url = trailingslashit($url);
+  $rel = trailingslashit($rel);
+  if ((strcasecmp($url, $rel) === 0) || sagextra_root_relative_url($url) == $rel) { 
+    return true; 
+  } else {
+    return false;
+  }
+}
+
+add_filter('wp_nav_menu_args', 'sagextra_nav_menu_args');
 add_filter('nav_menu_item_id', '__return_null');
