35f5c851ecc95358939bb47c9f4d082454131b39
Michael Romero <gitcomments@dimensionsix.net>
2015-05-20 07:46:18 -0400

    change the loader and add namespaces

--- modules/navwalker.php
+++ modules/navwalker.php
@@ -2,15 +2,8 @@
 
 namespace D6D\Sagextras\Navwalker;
 /**
- * Restore the Roots Bootstrap Navwalker
+ * Restore the Roots Bootstrap Navwalker for a cleaner Bootstrap menu 
  *
- * Walker_Nav_Menu (WordPress default) example output:
- *   <li id="menu-item-8" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-8"><a href="/">Home</a></li>
- *   <li id="menu-item-9" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9"><a href="/sample-page/">Sample Page</a></l
- *
- * Sagextra_Nav_Walker example output:
- *   <li class="menu-home"><a href="/">Home</a></li>
- *   <li class="menu-sample-page"><a href="/sample-page/">Sample Page</a></li>
  */
 class Sagextra_Nav_Walker extends Walker_Nav_Menu {
   private $cpt; // Boolean, is current post a custom post type.
--- sagextras.php
+++ sagextras.php
@@ -15,11 +15,11 @@ namespace D6D\Sagextras;
 
 function load_modules() {
   	if (current_theme_supports('se-navwalker')) {
-  		require_once __DIR__ . '/modules/se-navwalker.php';
+  		require_once __DIR__ . '/modules/navwalker.php';
   	}
 
   	if (current_theme_supports('se-gallery')) {
-  		require_once __DIR__ . '/modules/se-gallery.php';
+  		require_once __DIR__ . '/modules/gallery.php';
   	}
 }
 add_action('after_setup_theme', __NAMESPACE__ . '\\load_modules');
